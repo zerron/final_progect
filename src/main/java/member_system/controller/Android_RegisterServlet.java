1642346d3ed3e915b40737a1fc409076d72f0b97
zerron <ben.015237@gmail.com>
2016-09-19 16:39:07 +0800

    add android sign_up servlet

--- src/main/java/member_system/controller/Android_RegisterServlet.java
+++ src/main/java/member_system/controller/Android_RegisterServlet.java
@@ -3,6 +3,8 @@ package member_system.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,41 +15,43 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import member_system.model.CheckService;
 import member_system.model.CodingService;
 import member_system.model.MemberBean;
 import member_system.model.MemberDAO;
 
-@WebServlet("/Android/LoginServlet")
-public class Android_LoginServlet extends HttpServlet {
+@WebServlet("/Android/RegisterServlet")
+public class Android_RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-  
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		// 讀取Android端輸入的會員帳號密碼
+		// 讀取Android端輸入的會員註冊資料
 		BufferedReader br = request.getReader();
 		StringBuilder jsonIn = new StringBuilder();
 		String line = null;
 		while ((line = br.readLine()) != null) {
 			jsonIn.append(line);
 		}
-		MemberBean logIn = new Gson().fromJson(jsonIn.toString(), MemberBean.class);
-		String code = CodingService.encryptString(logIn.getPassword().trim());
-		// 查詢Database內對應的會員資料
-		MemberBean resultMember = new MemberDAO().selectByMemberid(logIn.getMemberId());
-		// 檢查會員帳號密碼
-		String error = "有效帳號";
-		if(resultMember == null) {
-			error = "此為無效帳號";
-		} else if(!code.equals(resultMember.getPassword())) {
-			error = "密碼輸入錯誤";
+		MemberBean register = new Gson().fromJson(jsonIn.toString(), MemberBean.class);
+		// 檢查會員註冊資料
+		JsonObject jsonOut = new JsonObject();
+		// 會員帳號是否重複
+		Map<String, String> error = CheckService.memberIsExist(register).getError();
+		if(error.isEmpty()) {
+			// 會員密碼加密
+			register.setPassword(CodingService.encryptString(register.getPassword().trim()));
+			// insert會員註冊資料至Database
+			new MemberDAO().insert(register);
+			
+			jsonOut.addProperty("errorMsg", "會員註冊成功");
+		} else {
+			jsonOut.addProperty("errorMsg", "帳號重複");
 		}
 		// 傳回檢查結果
-		JsonObject jsonOut = new JsonObject();
-		jsonOut.addProperty("errorMsg", error);
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println(jsonOut.toString());
-		
 	}
 
 }
